3158718e6fd6d2d2328c76983a53da6f80a0416e
Reinis Viļumsons <166484776+23DP4RVilu@users.noreply.github.com>
2025-04-02 09:47:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,110 +1,109 @@
 const cvs = document.querySelector("canvas");
-const c = cvs.getContext("2d");
+ const c = cvs.getContext("2d");
+ 
+ cvs.width = window.innerWidth;
+ cvs.height = window.innerHeight;
+ 
+ window.addEventListener("resize", function() {
+   cvs.width = window.innerWidth;
+   cvs.height = window.innerHeight;
+ });
+ 
+ let mouse = {
+   x: undefined,
+   y: undefined
+ };
+ 
+ window.addEventListener("mousemove", function(e) {
+   mouse.x = event.x;
+   mouse.y = event.y;
+ });
+ 
+ class Diamond {
+   constructor(x, y, dx, dy, width) {
+     this.x = x;
+     this.y = y;
+     this.dx = dx;
+     this.dy = dy;
+     this.width = width;
+     this.minWidth = width;
+     this.maxWidth = width * 10;
 
-cvs.width = window.innerWidth;
-cvs.height = window.innerHeight;
-
-window.addEventListener("resize", function() {
-  cvs.width = window.innerWidth;
-  cvs.height = window.innerHeight;
-});
-
-let mouse = {
-  x: undefined,
-  y: undefined
-};
-
-window.addEventListener("mousemove", function(e) {
-  mouse.x = event.x;
-  mouse.y = event.y;
-});
-
-class Diamond {
-  constructor(x, y, dx, dy, width) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.width = width;
-    this.minWidth = width;
-    this.maxWidth = width * 10;
-
-    let colorArray = ["#8a2be2", "#090c0b", "#dc143c", "#267368", "#ff00ff"];
-
-    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
-  }
-
-  draw = () => {
-    c.beginPath();
-    c.moveTo(this.x, this.y);
-    c.lineTo(this.x - this.width / 1, this.y);
-    c.lineTo(this.x, this.y + this.width / 1);
-    c.lineTo(this.x + this.width / 1, this.y);
-    c.lineTo(this.x, this.y - this.width / 1);
-    c.lineTo(this.x - this.width / 1, this.y);
-    c.closePath();
-
-    c.fillStyle = this.color;
-    c.fill();
-
-    this.update();
-  };
-
-  update = () => {
-    if (
-      this.x + this.width / 1 >= window.innerWidth ||
-      this.x - this.width / 1 <= 0
-    ) {
-      this.dx = -this.dx;
-    }
-
-    if (
-      this.y + this.width / 1 >= window.innerHeight ||
-      this.y - this.width / 1 <= 0
-    ) {
-      this.dy = -this.dy;
-    }
-
-    this.x += this.dx;
-    this.y += this.dy;
-
-    // interactivity
-    if (
-      mouse.x - this.x < 80 &&
-      mouse.x - this.x > -80 &&
-      mouse.y - this.y < 80 &&
-      mouse.y - this.y > -80 &&
-      this.width < this.maxWidth
-    ) {
-      this.width += 1;
-      this.x -= 1;
-      this.y -= 1;
-    } else if (this.width > this.minWidth) {
-      this.width -= 1;
-      this.x += 1;
-      this.y += 1;
-    }
-  };
-}
-
-let diamondArray = [];
-
-for (let i = 0; i < 400; i++) {
-  let width = Math.random() * 20 + 4;
-  let x = Math.random() * window.innerWidth;
-  let dx = (Math.random() - 0.5) * 1;
-  let y = Math.random() * window.innerHeight;
-  let dy = (Math.random() - 0.5) * 1;
-  diamondArray.push(new Diamond(x, y, dx, dy, width));
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-  diamondArray.forEach(diamond => {
-    diamond.draw();
-  });
-}
-
-animate();
+     let colorArray = ["#8a2be2", "#090c0b", "#dc143c", "#267368", "#ff00ff"];
+ 
+     this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+   }
+ 
+   draw = () => {
+     c.beginPath();
+     c.moveTo(this.x, this.y);
+     c.lineTo(this.x - this.width / 1, this.y);
+     c.lineTo(this.x, this.y + this.width / 1);
+     c.lineTo(this.x + this.width / 1, this.y);
+     c.lineTo(this.x, this.y - this.width / 1);
+     c.lineTo(this.x - this.width / 1, this.y);
+     c.closePath();
+ 
+     c.fillStyle = this.color;
+     c.fill();
+ 
+     this.update();
+   };
+ 
+   update = () => {
+     if (
+       this.x + this.width / 1 >= window.innerWidth ||
+       this.x - this.width / 1 <= 0
+     ) {
+       this.dx = -this.dx;
+     }
+ 
+     if (
+       this.y + this.width / 1 >= window.innerHeight ||
+       this.y - this.width / 1 <= 0
+     ) {
+       this.dy = -this.dy;
+     }
+ 
+     this.x += this.dx;
+     this.y += this.dy;
+ 
+     // interactivity
+     if (
+       mouse.x - this.x < 80 &&
+       mouse.x - this.x > -80 &&
+       mouse.y - this.y < 80 &&
+       mouse.y - this.y > -80 &&
+       this.width < this.maxWidth
+     ) {
+       this.width += 1;
+       this.x -= 1;
+       this.y -= 1;
+     } else if (this.width > this.minWidth) {
+       this.width -= 1;
+       this.x += 1;
+       this.y += 1;
+     }
+   };
+ }
+ 
+ let diamondArray = [];
+ 
+ for (let i = 0; i < 400; i++) {
+   let width = Math.random() * 20 + 4;
+   let x = Math.random() * window.innerWidth;
+   let dx = (Math.random() - 0.5) * 1;
+   let y = Math.random() * window.innerHeight;
+   let dy = (Math.random() - 0.5) * 1;
+   diamondArray.push(new Diamond(x, y, dx, dy, width));
+ }
+ 
+ function animate() {
+   requestAnimationFrame(animate);
+   c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+ 
+   diamondArray.forEach(diamond => {
+     diamond.draw();
+   });
+ }
+ animate();
